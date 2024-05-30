ecf8a1d96793ab61c27166c19533a3c3db30167b
Dino Himaj <dinohimaj@rocketmail.com>
2024-05-31 00:46:07 +0200

    initialized project

---
+++ src/test/java/com/example/celebtitycontacts/CelebtityContactsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.celebtitycontacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CelebtityContactsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
