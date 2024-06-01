c34d50b101c63670d46ac8fde440b7949c42f406
Dino Himaj <dinohimaj@rocketmail.com>
2024-06-02 01:53:27 +0200

    add Contact Service and Repository

---
+++ src/main/java/com/example/celebritycontacts/Service/ContactService.java
@@ -0,0 +1,49 @@
+package com.example.celebritycontacts.Service;
+
+import com.example.celebritycontacts.Repository.ContactRepository;
+import com.example.celebritycontacts.Domain.Contact;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+import static com.example.celebritycontacts.constants.Constants.PHOTO_DIRECTORY;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+
+@Service
+@Slf4j
+@Transactional(rollbackOn = Exception.class)
+@RequiredArgsConstructor
+public class ContactService {
+    private final ContactRepository contactRepo;
+
+    public Page<Contact> getAllContacts(int page, int size) {
+        return contactRepo.findAll(PageRequest.of(page, size, Sort.by("name")));
+    }
+
+    public Contact getContact(String id) {
+        return contactRepo.findById(id).orElseThrow(() -> new RuntimeException("Contact Not Found"));
+    }
+
+    public Contact createContact(Contact contact) {
+        return contactRepo.save(contact);
+    }
+
+    public void deleteContact(Contact contact) {
+        //#TODO:implement this method
+    }
+
+
+}
